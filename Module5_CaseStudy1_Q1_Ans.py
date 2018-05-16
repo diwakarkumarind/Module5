62eb66aecfcd19ac3789e86de080ce7fb202d429
Diwakar Kumar <diwakarkumar.ind@gmail.com>
2018-05-16 14:43:24 +0530

    Find the highest rated movie in the Quest story type.

--- Module5_CaseStudy1_Q1_Ans.py
+++ Module5_CaseStudy1_Q1_Ans.py
@@ -1,6 +1,8 @@
 # Find the highest rated movie in the Quest story type.
+
 import numpy as np
 import pandas as pd
+
 dataset=pd.read_csv('c:\\temp\\HollywoodMovies.csv')
 selected_data=dataset.loc[:,['Movie','Story','RottenTomatoes']]
 df=pd.DataFrame(selected_data)
